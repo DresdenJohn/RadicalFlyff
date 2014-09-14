09a0c2c8efc62203055dc3d5df0bdc5fd0faa01d
Charles Spanky <kingspankyman@gmail.com>
2014-09-14 18:14:13 -0400

    Fixes to AllKill, Certifier.ini and Resource.txt

---
+++ Program/AllKill.bat
@@ -0,0 +1,7 @@
+TaskKill /f /im 1.AccountServer.exe
+TaskKill /f /im 2.DatabaseServer.exe
+TaskKill /f /im 3.CoreServer.exe
+TaskKill /f /im 4.Certifier.exe
+TaskKill /f /im 5.LoginServer.exe
+TaskKill /f /im 6.CacheServer.exe
+TaskKill /f /im 7.WorldServer.exe
